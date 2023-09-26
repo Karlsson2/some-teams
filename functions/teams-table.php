3b70f16f3a5b71136ea38db3753cc10def165037
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2023-09-26 11:05:31 +0200

    skapat alla filer och länkat ihop dem

---
+++ functions/teams-table.php
@@ -0,0 +1,2 @@
+<?php
+    echo "<p>teams table</p>";
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    require __DIR__ . "/data/data.php";
+    require __DIR__ . "/header.php";
+    require __DIR__ . "/functions/cities.php";
+    require __DIR__ . "/functions/teams-table.php";
+    require __DIR__ . "/functions/total-teams.php";
+    require __DIR__ . "/footer.php"; 
+?>
