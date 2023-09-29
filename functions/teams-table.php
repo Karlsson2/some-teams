bda4734bc37f72a8571eb1ee085bc368b3321482
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2023-09-29 11:55:08 +0200

    formatted some files

--- functions/teams-table.php
+++ functions/teams-table.php
@@ -1,22 +1,22 @@
 <?php
-    require __DIR__ . "../../data/data.php";
+require __DIR__ . "../../data/data.php";
 ?>
 
 <!-- foreach loop that goes through each of the teams within the teams array and fetches the information within the array!-->
 
-<?php foreach($teams as $key => $team) : ?>
+<?php foreach ($teams as $key => $team) : ?>
     <tr>
-        <td data-label="Team Name"><?= $key?></td>
-        <td data-label="League"><?= $team['league']?></td>
-        <td data-label="Last time champions"><?= $team['last-time-champions']?></td>
-        <td data-label="City"><?= $team['city']?></td>
+        <td data-label="Team Name"><?= $key ?></td>
+        <td data-label="League"><?= $team['league'] ?></td>
+        <td data-label="Last time champions"><?= $team['last-time-champions'] ?></td>
+        <td data-label="City"><?= $team['city'] ?></td>
         <!-- if statement to catch the arrays that dont contain nickname keys -->
-        <?php if(array_key_exists("nickname", $team)) : ?>
-            <td data-label="Nickname"><?= $team['nickname']?></td>
-        <?php else :?>
+        <?php if (array_key_exists("nickname", $team)) : ?>
+            <td data-label="Nickname"><?= $team['nickname'] ?></td>
+        <?php else : ?>
             <!--if they dont have the nickname key print an empty table cell -->
             <td></td>
-        <?php endif;?>
-        <td data-label="Website"><a href="<?= $team['url']?>"><?= $key?></td>
+        <?php endif; ?>
+        <td data-label="Website"><a href="<?= $team['url'] ?>"><?= $key ?></td>
     </tr>
 <?php endforeach; ?>
--- header.php
+++ header.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>Some teams</title>
-    <link rel="icon" type="image/x-icon" href="/images/premier.ico">
-    <link rel="stylesheet" href="styles.css"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;800&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <header>
-      <h1>Some Teams</h1>
-    </header>
-    
+
+<head>
+  <meta charset="utf-8" />
+  <title>Some teams</title>
+  <link rel="icon" type="image/x-icon" href="/images/premier.ico">
+  <link rel="stylesheet" href="styles.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <h1>Some Teams</h1>
+  </header>
