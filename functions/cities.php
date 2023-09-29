cc61556a3f5df00419ef0d7386dd4dff64d18dc5
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2023-09-29 11:32:18 +0200

    changed some formatting

--- functions/cities.php
+++ functions/cities.php
@@ -1,11 +1,9 @@
 <?php
-    require __DIR__ . "../../data/data.php";
-    /*grabbing each of the cities and putting them in an array*/
-    $cities = array();
-    foreach ($teams as $team) {
-        $cities[] = $team['city'];
-    }
-    /*removing duplicated using array_unique*/
-    $uniqueCities = array_unique($cities);
-?>
-
+require __DIR__ . "../../data/data.php";
+/*grabbing each of the cities and putting them in an array*/
+$cities = array();
+foreach ($teams as $team) {
+    $cities[] = $team['city'];
+}
+/*removing duplicated using array_unique*/
+$uniqueCities = array_unique($cities);
